aca8f0a384ff30a7e1674bf2ac5a7a4c9170da61
gm-28 <guilhermemoreira2000@gmail.com>
2023-06-15 04:06:02 +0100

    update

--- ibrdtn/tools/src/dtnrecv_v2.cpp
+++ ibrdtn/tools/src/dtnrecv_v2.cpp
@@ -18,16 +18,19 @@
  * limitations under the License.
  *
  */
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <libssh/libssh.h>
-#include <libssh/sftp.h>
 #include <iostream>
-#include "keyconfig.h" 
-#include <fcntl.h>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cerrno>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #include "config.h"
+
 #include <ibrdtn/api/Client.h>
 #include <ibrcommon/net/socket.h>
 #include <ibrcommon/net/socketstream.h>
@@ -42,6 +45,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libssh/libssh.h>
+
 
 void print_help()
 {
@@ -82,254 +86,30 @@ void term(int signal)
 	}
 }
 
-bool serializeBundleToFile(const std::string filename, const dtn::data::Bundle bundle) {
-    // Open the output file stream
-    std::ofstream outputFile(filename, std::ios::binary);
-    if (!outputFile.is_open()) {
-        std::cerr << "Error opening output file" << std::endl;
+bool deserializeBundleFromFile(const std::string localFilePath, dtn::data::Bundle& bundle) {
+    // Open the input file stream
+    std::ifstream inputFile(localFilePath, std::ios::binary);
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening input file" << std::endl;
         return false;
     }
 
     try {
-        // Create a DefaultSerializer object with the output stream
-        dtn::data::DefaultSerializer serializer(outputFile);
+        // Create a DefaultDeserializer object with the input stream
+        dtn::data::DefaultDeserializer deserializer(inputFile);
 
-        // Serialize the bundle and write it to the file
-        serializer << bundle;
+        // Deserialize the bundle from the file
+        deserializer >> bundle;
 
-        // Close the output file stream
-        outputFile.close();
+        // Close the input file stream
+        inputFile.close();
 
         return true;
     } catch (const std::exception& e) {
         std::cerr << "Error serializing bundle: " << e.what() << std::endl;
-        outputFile.close();
+        inputFile.close();
         return false;
     }
-}
-void disconnect(ssh_channel channel) {
-    // Close the channel
-    ssh_channel_send_eof(channel);
-    ssh_channel_close(channel);
-    ssh_channel_free(channel);
-}
-
-/**
- * Transfer a file to a remote server using SFTP.
- * 
- * @param session         The SSH session established with the remote server.
- * @param localFilePath   The path to the local file to transfer.
- * @param remoteFilePath  The path to the remote file on the server.
- * @return                True if the file transfer is successful, false otherwise.
- */
-bool transferFileToRemote(ssh_session session, const std::string& localFilePath, const std::string& remoteFilePath) {
-    // Open the file for reading
-    std::ifstream file(localFilePath, std::ios::binary);
-    if (!file.is_open()) {
-        std::cerr << "Error opening local file" << std::endl;
-        return false;
-    }
-
-    // Get the size of the file
-    file.seekg(0, std::ios::end);
-    std::streampos fileSize = file.tellg();
-    file.seekg(0, std::ios::beg);
-
-    // Allocate a buffer to store the file data
-    char* buffer = new char[fileSize];
-
-    // Read the file data into the buffer
-    file.read(buffer, fileSize);
-    file.close();
-
-    // Create an SFTP session
-    sftp_session sftp = sftp_new(session);
-    if (sftp == nullptr) {
-        std::cerr << "Error creating SFTP session: " << ssh_get_error(session) << std::endl;
-        delete[] buffer;
-        return false;
-    }
-
-    // Initialize the SFTP session
-    int rc = sftp_init(sftp);
-    if (rc != SSH_OK) {
-        std::cerr << "Error initializing SFTP session: " << ssh_get_error(session) << std::endl;
-        sftp_free(sftp);
-        delete[] buffer;
-        return false;
-    }
-
-    // Open a file on the remote server for writing
-    sftp_file remoteFile = sftp_open(sftp, remoteFilePath.c_str(), O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (remoteFile == nullptr) {
-        std::cerr << "Error opening remote file: " << ssh_get_error(session) << std::endl;
-        sftp_free(sftp);
-        delete[] buffer;
-        return false;
-    }
-
-    // Write the file data to the remote file
-    rc = sftp_write(remoteFile, buffer, fileSize);
-    if (rc < 0) {
-        std::cerr << "Error writing to remote file: " << ssh_get_error(session) << std::endl;
-        sftp_close(remoteFile);
-        sftp_free(sftp);
-        delete[] buffer;
-        return false;
-    }
-
-    // Close the remote file and free the SFTP session
-    sftp_close(remoteFile);
-    sftp_free(sftp);
-    delete[] buffer;
-
-    return true;
-}
-
-void sendBundle(ssh_session session, const std::string localFilePath, const std::string remoteFilePath, const std::string destination) {
-    bool transferSuccess = transferFileToRemote(session, localFilePath, remoteFilePath);
-    if (!transferSuccess) {
-        std::cerr << "Error transferring file: " << localFilePath << std::endl;
-        return;
-    }
-
-    std::string command = "dtnsend_v2 " + destination + " " + remoteFilePath;
-    std::cout << command.c_str();
-    std::cout << "\n";
-
-    ssh_channel channel_cmd = ssh_channel_new(session);
-    int rc = ssh_channel_open_session(channel_cmd);
-    if (rc != SSH_OK) {
-        fprintf(stderr, "Error opening channel: %s\n", ssh_get_error(session));
-        ssh_channel_free(channel_cmd);
-        return;
-    }
-
-    rc = ssh_channel_request_exec(channel_cmd, command.c_str());
-    if (rc != SSH_OK) {
-        fprintf(stderr, "Error executing command: %s\n", ssh_get_error(session));
-        ssh_channel_free(channel_cmd);
-        return;
-    }
-
-    int c = 0;
-    char buffer2[1024];
-    int nbytes;
-    while ((nbytes = ssh_channel_read(channel_cmd, buffer2, sizeof(buffer2), 0)) > 0) {
-        fwrite(buffer2, 1, nbytes, stdout);
-        c++;
-        if (c == 5) break;
-    }
-
-    disconnect(channel_cmd);
-}
-/**
- * Splits aa BLOB into smaller chunks of a given size and returns a vector of BLOB references.
- * 
- * @param blob The BLOB to be split.
- * @param chunkSize The size of each chunk in bytes.
- * 
- * @return A vector of BLOB references, each representing a chunk of the original BLOB.
- */
-std::vector<ibrcommon::BLOB::Reference> splitBlob(const ibrcommon::BLOB::Reference &blob, const size_t chunkSize)
-{
-    std::vector<ibrcommon::BLOB::Reference> chunks;
-
-	// Create a new BLOB reference for this chunk
-	ibrcommon::BLOB::Reference blobref = blob;
-
-    // Get an iostream for the BLOB
-	ibrcommon::BLOB::iostream io = blobref.iostream();
-
-    size_t remainingSize = blob.size();
-    size_t offset = 0;
-
-    while (remainingSize > 0) {
-        size_t chunkLength = std::min(chunkSize, remainingSize);
-
-        ibrcommon::BLOB::Reference chunkBlob = ibrcommon::BLOB::create();
-        ibrcommon::BLOB::iostream chunkStream = chunkBlob.iostream();
-
-        (*io).seekg(offset, std::ios::beg);
-        ibrcommon::BLOB::copy(*chunkStream, *io, chunkLength);
-
-        chunks.push_back(chunkBlob);
-
-        remainingSize -= chunkLength;
-        offset += chunkLength;
-    }
-
-    return chunks;
-}
-
-
-static int auth_keyfile(ssh_session session, const char* keyfile) {
-    ssh_key key = NULL;
-    char pubkey[256] = {0}; // Public key file path
-    int rc;
-
-    snprintf(pubkey, sizeof(pubkey), "%s.pub", keyfile);
-
-    // Import public key file
-    rc = ssh_pki_import_pubkey_file(pubkey, &key);
-    if (rc != SSH_OK) {
-        fprintf(stderr, "Error importing public key file: %s\n", ssh_get_error(session));
-        return SSH_AUTH_DENIED;
-    }
-
-    // Try public key authentication
-    rc = ssh_userauth_try_publickey(session, NULL, key);
-    ssh_key_free(key);
-    if (rc != SSH_AUTH_SUCCESS) {
-        fprintf(stderr, "Public key authentication failed: %s\n", ssh_get_error(session));
-        return SSH_AUTH_DENIED;
-    }
-
-    // Import private key file
-    rc = ssh_pki_import_privkey_file(keyfile, NULL, NULL, NULL, &key);
-    if (rc != SSH_OK) {
-        fprintf(stderr, "Error importing private key file: %s\n", ssh_get_error(session));
-        return SSH_AUTH_DENIED;
-    }
-
-    // Authenticate with public key
-    rc = ssh_userauth_publickey(session, NULL, key);
-    ssh_key_free(key);
-    if (rc != SSH_AUTH_SUCCESS) {
-        fprintf(stderr, "Public key authentication failed: %s\n", ssh_get_error(session));
-        return SSH_AUTH_DENIED;
-    }
-
-    return rc;
-}
-
-static ssh_session start_session(const char* host, const char* user, const char* keyfile, const char* port) {
-    ssh_session session = ssh_new();
-    if (session == NULL) {
-        fprintf(stderr, "Error creating SSH session\n");
-        exit(EXIT_FAILURE);
-    }
-
-    ssh_options_set(session, SSH_OPTIONS_HOST, host);
-    ssh_options_set(session, SSH_OPTIONS_USER, user);
-    ssh_options_set(session, SSH_OPTIONS_PORT_STR, port);
-
-    int rc = ssh_connect(session);
-    if (rc != SSH_OK) {
-        fprintf(stderr, "Error connecting to virtual machine: %s\n", ssh_get_error(session));
-        ssh_free(session);
-        exit(EXIT_FAILURE);
-    }
-
-    rc = auth_keyfile(session, keyfile);
-    if (rc != SSH_AUTH_SUCCESS) {
-        fprintf(stderr, "Error authenticating with virtual machine\n");
-        ssh_disconnect(session);
-        ssh_free(session);
-        exit(EXIT_FAILURE);
-    }
-
-    return session;
 }
 
 int main(int argc, char *argv[])
@@ -348,7 +128,7 @@ int main(int argc, char *argv[])
 
 	int ret = EXIT_SUCCESS;
 	std::string filename = "";
-	std::string name = "dtnRecv";
+	std::string name = "filetransfer";
 	dtn::data::EID group;
 	int timeout = 0;
 	int count   = 1;
@@ -419,6 +199,10 @@ int main(int argc, char *argv[])
 		ibrcommon::Logger::addStream(std::cerr, loglevel, logopts);
 	}
 
+	std::string sequencenumber;
+	std::string timestamp;
+	std::string sourceeid;
+
 	try {
 		// Create a stream to the server using TCP.
 		ibrcommon::clientsocket *sock = NULL;
@@ -458,28 +242,33 @@ int main(int argc, char *argv[])
 			file.exceptions(std::ios::badbit | std::ios::eofbit);
 		}
 
-        EID addr = EID("dtn://moreira-XPS-15-9570");
-
 		for(h = 0; h < count; ++h)
 		{
 			// receive the bundle
 			dtn::data::Bundle b = client.getBundle(timeout);
-            //b.source = addr;
 
+			timestamp = b.timestamp.toString().c_str();
+			sequencenumber = b.sequencenumber.toString().c_str();
+			sourceeid = b.source.getString().c_str();
 			// get the reference to the blob
 			ibrcommon::BLOB::Reference ref = b.find<dtn::data::PayloadBlock>().getBLOB();
 
-            // Open the output file stream
-            std::ofstream outputFile("Receiver/bundle.bin", std::ios::binary);
+			// write the data to output
+			if (_stdout)
+			{
+				std::cout << ref.iostream()->rdbuf() << std::flush;
+			}
+			else
+			{
+				// write data to temporary file
+				try {
+					std::cout << "Bundle received (" << (h + 1) << ")." << std::endl;
 
-            // Create a DefaultSerializer object with the output stream
-            dtn::data::DefaultSerializer serializer(outputFile);
+					file << ref.iostream()->rdbuf();
+				} catch (const std::ios_base::failure&) {
 
-            // Serialize the bundle and write it to the file
-            serializer << b;
-            
-            // Close the output file stream
-            outputFile.close();
+				}
+			}	
 		}
 
 		if (!_stdout)
@@ -488,9 +277,22 @@ int main(int argc, char *argv[])
 			std::cout << "done." << std::endl;
 		}
 
+		dtn::data::Bundle b1;
+		deserializeBundleFromFile("ibrdtn/ibrdtn/tools/src/Receiver/bundle.bin",b1);
+		dtn::data::BundleID& id = b1;
+		int sequence = std::stoi(id.sequencenumber.toString().c_str());
+		std::cout << "Sequence of transferred file " << sequencenumber << " Timestamp " << timestamp << " Source " << sourceeid << "\n";
+
 		// Shutdown the client connection.
 		client.close();
 
+    	std::string command = "python3 ibrdtn/ibrdtn/tools/src/removebundle2.py " + timestamp + " " + sequencenumber + " " + sourceeid;
+    	int result = system(command.c_str()); 		
+		if (result == 0){
+			std::cout << "Bundle freed successfully!"<< std::endl;
+		}else{
+			std::cout << "Bundle freed unsuccessfully!"<< std::endl;	
+		}
 		// close the tcp connection
 		conn.close();
 	} catch (const dtn::api::ConnectionTimeoutException&) {
@@ -503,7 +305,6 @@ int main(int argc, char *argv[])
 	} catch (const std::exception &ex) {
 		std::cerr << "Error: " << ex.what() << std::endl;
 		ret = EXIT_FAILURE;
-	}
-
+	}		
 	return ret;
 }
---
+++ ibrdtn/tools/src/removebundle2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import socket
+import sys
+
+arguments = sys.argv
+script_name = arguments[0]
+timestamp = arguments[1]
+seq_nr = arguments[2]
+source_eid = arguments[3]
+
+HOST = 'localhost'
+PORT = 4550
+
+''' Create a socket '''
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as msg:
+    sys.stderr.write("[ERROR] %s\n" % msg)
+    sys.exit(1)
+
+''' Connect to the daemon '''
+try:
+    sock.connect((HOST, PORT))
+    fsock = sock.makefile()
+except socket.error as msg:
+    sys.stderr.write("[ERROR] %s\n" % msg)
+    sys.exit(1)
+
+''' Read the header '''
+fsock.readline()
+
+''' Switch into extended management protocol mode '''
+sock.send(b"protocol extended\n")
+
+''' Read the protocol switch '''
+fsock.readline()
+
+''' Add registration '''
+sock.send(b"registration add dtn://moreira1-VirtualBox/ackRecv\n")
+
+''' Load bundle based on timestamp, sequence number, and source EID '''
+load_command = "bundle load {} {} {}\n".format(timestamp, seq_nr, source_eid)
+sock.send(load_command.encode())
+
+''' Free bundle from queue '''
+sock.send(b"bundle free\n")
+
+''' Close the socket '''
+sock.close()
+
+''' Finally exit the script '''
+sys.exit(0)
+
+
