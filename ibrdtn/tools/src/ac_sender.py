35c5667b1f3c1bca388773c403a2daa2aec17d2c
gm-28 <guilhermemoreira2000@gmail.com>
2023-06-19 16:40:12 +0100

    acoustic sender

---
+++ ibrdtn/tools/src/ac_sender.py
@@ -0,0 +1,31 @@
+import sys
+from fjagepy import Gateway, MessageClass
+
+# Check if the correct number of command-line arguments is provided
+if len(sys.argv) != 5:
+    print("Usage: python3 ac_sender.py <source_ip_address> <port> <destination_address> <timeout(ms)>")
+    sys.exit(1)
+
+ip_address = sys.argv[1]
+port = int(sys.argv[2])
+node = int(sys.argv[3])
+timeout = int(sys.argv[4])
+
+# Create the Gateway object
+gw = Gateway(ip_address, port)
+
+# Define the message class
+ShellExecReq = MessageClass('org.arl.fjage.shell.ShellExecReq')
+
+# Get the agent for the shell service
+shell = gw.agentForService('org.arl.fjage.shell.Services.SHELL')
+
+# Create the ShellExecReq message with the desired command
+cmd = 'fput {}, \'bundleac.bin\''.format(node)
+req = ShellExecReq(recipient=shell, cmd=cmd)
+
+rsp = gw.request(req, timeout)
+print(rsp)
+
+# Close the gateway connection
+gw.close()
